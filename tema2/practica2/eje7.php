dea759b9ced777245293fc57c3f0ac2768694eb3
unknown <alexander.br159@gmail.com>
2022-10-13 10:26:14 +0200

    practica2 terminada

--- tema2/practica2/eje7.php
+++ tema2/practica2/eje7.php
@@ -7,94 +7,51 @@ include_once($_SERVER["DOCUMENT_ROOT"] . "/cabecera.php");
 
 		<?php
 		$biblioteca = array(
-			array("isbn" => 114, "titulo" => "Cuento de hadas", "descripcion" => "Stephen King regresa a la fantasía por todo lo alto con una novela magnífica sobre un inesperado héroe que deberá tomar parte en la épica batalla entre el bien y el mal.", "categoria" => "terror", "editorial" => "penguin random house", "foto" => "./img/1.gif", "precio" => 12.5),
-			array("isbn" => 213, "titulo" => "Después", "descripcion" => "En ocasiones, para crecer debes vencer a tus demonios. Jamie Conklin, el único hijo de una madre soltera, solo quiere tener una infancia normal. Sin embargo, nació con una habili...", "categoria" => "terror", "editorial" => "penguin random house", "foto" => "./img/2.gif", "precio" => 15.4),
-			array("isbn" => 312, "titulo" => "Billy Summers", "descripcion" => "Billy Summers es un asesino a sueldo y el mejor en lo suyo, pero tiene una norma: solo acepta un encargo si su objetivo es realmente mala persona. Ahora...", "categoria" => "scifi", "editorial" => "penguin random house", "foto" => "./img/3.gif", "precio" => 20.1),
-			array("isbn" => 411, "titulo" => "El misterio de Salem's Lot", "descripcion" => "Salem's Lot es un pueblo tranquilo donde nunca pasa nada. O quizás esto son solo apariencias, pues lo cierto es que sí se están sucediendo diversos hechos misteriosos, incluso escalofriantes... ", "categoria" => "scifi", "editorial" => "penguin random house", "foto" => "./img/4.gif", "precio" => 17.8),
-			array("isbn" => 510, "titulo" => "El visitante", "descripcion" => "Un niño de once años ha sido brutalmente asesinado. Todas las pruebas apuntan a uno de los ciudadanos más queridos de Flint City: Terry Maitland, entrenador en la liga infantil, profesor de literatura, marido ejemplar y padre de dos niñas.", "categoria" => "historia", "editorial" => "penguin random house", "foto" => "./img/5.gif", "precio" => 19, 2),
-			array("isbn" => 69, "titulo" => "La última misión de Gwendy", "descripcion" => "La caja de botones de Gwendy. Cuando tenía doce años, Gwendy Peterson conoció a Richard Farris, un tipo misterioso que le pidió que cuidara de una extraña caja de botones. Ahor...", "categoria" => "historia", "editorial" => "penguin random house", "foto" => "./img/6.gif", "precio" => 30.1),
-			array("isbn" => 78, "titulo" => "Carrie", "descripcion" => "Adolescencia, venganza y sangre, todo un hito en la literatura popular. Carrie, una joven de apariencia insignificante, acosada por sus compañeras de instituto, vive con su madre, una fanática religiosa.", "categoria" => "novela", "editorial" => "penguin random house", "foto" => "./img/7.gif", "precio" => 13.2),
-			array("isbn" => 87, "titulo" => "Apocalipsis", "descripcion" => "El libro en el que se basa la serie de The Stand Un virus gripal, creado artificialmente como posible arma bacteriológica, se extiende por Estados Unidos y el mundo, provoca...", "categoria" => "novela", "editorial" => "penguin random house", "foto" => "./img/8.gif", "precio" => 11.5),
-			array("isbn" => 96, "titulo" => "La història de la Lisey", "descripcion" => "Han pasado dos años desde que Lisey perdió a su marido, con quien había compartido veinticinco años de matrimonio. Scott era un novelista premiado, con gran éxito, y un hombre...", "categoria" => "terror", "editorial" => "penguin random house", "foto" => "./img/9.gif", "precio" => 16.2),
-			array("isbn" => 105, "titulo" => "Misery", "descripcion" => "Estaba loca, pero él la necesitaba. Misery Chastain ha muerto. Paul Sheldon la ha matado. Con alivio y hasta con alegría. Misery lo ha hecho rico. Porque Misery es la heroína que ha protagonizado sus exitosos libros.", "categoria" => "novela", "editorial" => "penguin random house", "foto" => "./img/10.gif", "precio" => 23.5),
-			array("isbn" => 114, "titulo" => "La cúpula", "descripcion" => "Una historia apocalíptica e hipnótica. Totalmente fascinante. Lo mejor de Stephen King. Es una soleada mañana de otoño en la pequeña ciudad de Chester's Mill. Claudette Sanders disfruta de su clase de vuelo y Dale Barbara, Barbie para los amigos, hace autostop en las afueras.", "categoria" => "terror", "editorial" => "penguin random house", "foto" => "./img/11.gif", "precio" => 29.8),
-			array("isbn" => 123, "titulo" => "La larga marcha", "descripcion" => "Una inquietante novela futurista donde la realidad supera a la fantasía más terrorífica. El escenario: una sociedad ultraconservadora que ha llevado al paroxismo sus rasgos más perversos, dominada por un estado policial.", "categoria" => "scifi", "editorial" => "penguin random house", "foto" => "./img/12.gif", "precio" => 14.2),
-			array("isbn" => 132, "titulo" => "El fugitivo", "descripcion" => "Una angustiante novela del maestro de terror Stephen King sobre un aterrador futuro donde la televisión es la única realidad. A mediados del siglo XXI, un concurso televisivo, cuyo principal atractivo es la muerte de los participantes, bate récords de audiencia.", "categoria" => "historia", "editorial" => "penguin random house", "foto" => "./img/13.gif", "precio" => 19.1),
-			array("isbn" => 141, "titulo" => "El Instituto", "descripcion" => "En mitad de la noche en un barrio tranquilo de Minneapolis raptan a Luke Ellis, de doce años, tras haber asesinado a sus padres. Una operación que dura menos de dos minutos. Luke se despierta en l...", "categoria" => "novela", "editorial" => "penguin random house", "foto" => "./img/14.gif", "precio" => 13.9),
-			array("isbn" => 150, "titulo" => "It", "descripcion" => "¿Quién o qué mutila y mata a los niños de un pequeño pueblo norteamericano?¿Por qué llega cíclicamente el horror a Derry en forma de un payaso siniestro que va sembrando la destrucción a su paso?", "categoria" => "terror", "editorial" => "penguin random house", "foto" => "./img/15.gif", "precio" => 13, 9),
+			array("isbn" => 114, "titulo" => "Cuento de hadas", "descripcion" => "Stephen King regresa a la fantasía por todo lo alto con una novela magnífica sobre un inesperado héroe que deberá tomar parte en la épica batalla entre el bien y el mal.", "categoria" => "terror", "editorial" => "penguin random house", "foto" => "./img7/1.gif", "precio" => 12.5),
+			array("isbn" => 213, "titulo" => "Después", "descripcion" => "En ocasiones, para crecer debes vencer a tus demonios. Jamie Conklin, el único hijo de una madre soltera, solo quiere tener una infancia normal. Sin embargo, nació con una habili...", "categoria" => "terror", "editorial" => "penguin random house", "foto" => "./img7/2.gif", "precio" => 15.4),
+			array("isbn" => 312, "titulo" => "Billy Summers", "descripcion" => "Billy Summers es un asesino a sueldo y el mejor en lo suyo, pero tiene una norma: solo acepta un encargo si su objetivo es realmente mala persona. Ahora...", "categoria" => "scifi", "editorial" => "penguin random house", "foto" => "./img7/3.gif", "precio" => 20.1),
+			array("isbn" => 411, "titulo" => "El misterio de Salem's Lot", "descripcion" => "Salem's Lot es un pueblo tranquilo donde nunca pasa nada. O quizás esto son solo apariencias, pues lo cierto es que sí se están sucediendo diversos hechos misteriosos, incluso escalofriantes... ", "categoria" => "scifi", "editorial" => "penguin random house", "foto" => "./img7/4.gif", "precio" => 17.8),
+			array("isbn" => 510, "titulo" => "El visitante", "descripcion" => "Un niño de once años ha sido brutalmente asesinado. Todas las pruebas apuntan a uno de los ciudadanos más queridos de Flint City: Terry Maitland, entrenador en la liga infantil, profesor de literatura, marido ejemplar y padre de dos niñas.", "categoria" => "historia", "editorial" => "penguin random house", "foto" => "./img7/5.gif", "precio" => 19, 2),
+			array("isbn" => 69, "titulo" => "La última misión de Gwendy", "descripcion" => "La caja de botones de Gwendy. Cuando tenía doce años, Gwendy Peterson conoció a Richard Farris, un tipo misterioso que le pidió que cuidara de una extraña caja de botones. Ahor...", "categoria" => "historia", "editorial" => "penguin random house", "foto" => "./img7/6.gif", "precio" => 30.1),
+			array("isbn" => 78, "titulo" => "Carrie", "descripcion" => "Adolescencia, venganza y sangre, todo un hito en la literatura popular. Carrie, una joven de apariencia insignificante, acosada por sus compañeras de instituto, vive con su madre, una fanática religiosa.", "categoria" => "novela", "editorial" => "penguin random house", "foto" => "./img7/7.gif", "precio" => 13.2),
+			array("isbn" => 87, "titulo" => "Apocalipsis", "descripcion" => "El libro en el que se basa la serie de The Stand Un virus gripal, creado artificialmente como posible arma bacteriológica, se extiende por Estados Unidos y el mundo, provoca...", "categoria" => "novela", "editorial" => "penguin random house", "foto" => "./img7/8.gif", "precio" => 11.5),
+			array("isbn" => 96, "titulo" => "La història de la Lisey", "descripcion" => "Han pasado dos años desde que Lisey perdió a su marido, con quien había compartido veinticinco años de matrimonio. Scott era un novelista premiado, con gran éxito, y un hombre...", "categoria" => "terror", "editorial" => "penguin random house", "foto" => "./img7/9.gif", "precio" => 16.2),
+			array("isbn" => 105, "titulo" => "Misery", "descripcion" => "Estaba loca, pero él la necesitaba. Misery Chastain ha muerto. Paul Sheldon la ha matado. Con alivio y hasta con alegría. Misery lo ha hecho rico. Porque Misery es la heroína que ha protagonizado sus exitosos libros.", "categoria" => "novela", "editorial" => "penguin random house", "foto" => "./img7/10.gif", "precio" => 23.5),
+			array("isbn" => 114, "titulo" => "La cúpula", "descripcion" => "Una historia apocalíptica e hipnótica. Totalmente fascinante. Lo mejor de Stephen King. Es una soleada mañana de otoño en la pequeña ciudad de Chester's Mill. Claudette Sanders disfruta de su clase de vuelo y Dale Barbara, Barbie para los amigos, hace autostop en las afueras.", "categoria" => "terror", "editorial" => "penguin random house", "foto" => "./img7/11.gif", "precio" => 29.8),
+			array("isbn" => 123, "titulo" => "La larga marcha", "descripcion" => "Una inquietante novela futurista donde la realidad supera a la fantasía más terrorífica. El escenario: una sociedad ultraconservadora que ha llevado al paroxismo sus rasgos más perversos, dominada por un estado policial.", "categoria" => "scifi", "editorial" => "penguin random house", "foto" => "./img7/12.gif", "precio" => 14.2),
+			array("isbn" => 132, "titulo" => "El fugitivo", "descripcion" => "Una angustiante novela del maestro de terror Stephen King sobre un aterrador futuro donde la televisión es la única realidad. A mediados del siglo XXI, un concurso televisivo, cuyo principal atractivo es la muerte de los participantes, bate récords de audiencia.", "categoria" => "historia", "editorial" => "penguin random house", "foto" => "./img7/13.gif", "precio" => 19.1),
+			array("isbn" => 141, "titulo" => "El Instituto", "descripcion" => "En mitad de la noche en un barrio tranquilo de Minneapolis raptan a Luke Ellis, de doce años, tras haber asesinado a sus padres. Una operación que dura menos de dos minutos. Luke se despierta en l...", "categoria" => "novela", "editorial" => "penguin random house", "foto" => "./img7/14.gif", "precio" => 13.9),
+			array("isbn" => 150, "titulo" => "It", "descripcion" => "¿Quién o qué mutila y mata a los niños de un pequeño pueblo norteamericano?¿Por qué llega cíclicamente el horror a Derry en forma de un payaso siniestro que va sembrando la destrucción a su paso?", "categoria" => "terror", "editorial" => "penguin random house", "foto" => "./img7/15.gif", "precio" => 13, 9),
 		);
 
-		
+		function pintarLibros($array, $categoria){
+			$cont = 0;
+			foreach ($array as $value) {
+
+				if (strcmp($value["categoria"], $categoria) === 0 && $cont < 4) {
+					echo "<div class='card' style='width: 18rem; margin-left:40px;'>
+						  <img src='" . $value["foto"] . "' class='card-img-top' alt='...'>
+						  <div class='card-body'>
+							   <h5 class='card-title'>" . $value["titulo"] . "</h5>
+							<p class='card-text'>" . $value['descripcion'] . "</p>
+							<a href='#' class='btn btn-primary'>" . $value["precio"] . " €</a>
+						  </div>
+					</div>";
+					$cont++;
+				}
+			}
+		}
 		echo "<div class='container'>";
 		echo "<div class='row g-5'>";
-		echo"<h1>LIBRERIA JAROSO 2022</h1>";
-		echo"<h2>TERROR</h2>";
-
-		$cont=0;
-		$cont1=0;
-		$cont2=0;
-		$cont3=0;
-		
-		foreach ($biblioteca as $key => $value) {
-
-			if (strcmp($value["categoria"], "terror") === 0 && $cont<4) {
-				echo "<div class='card' style='width: 18rem; margin-left:40px;'>
-  					<img src='" . $value["foto"] . "' class='card-img-top' alt='...'>
-  					<div class='card-body'>
-   						<h5 class='card-title'>" . $value["titulo"] . "</h5>
-    					<p class='card-text'>" . $value['descripcion'] . "</p>
-    					<a href='#' class='btn btn-primary'>".$value["precio"]." €</a>
-  					</div>
-				</div>";
-				$cont++;
-			}
-		}
-		echo"<h2>HISTORIA</h2>";
-		foreach ($biblioteca as $key => $value) {
-
-			if (strcmp($value["categoria"], "historia") === 0 && $cont1<4) {
-				echo "<div class='card' style='width: 18rem; margin-left:40px;'>
-  					<img src='" . $value["foto"] . "' class='card-img-top' alt='...'>
-  					<div class='card-body'>
-   						<h5 class='card-title'>" . $value["titulo"] . "</h5>
-    					<p class='card-text'>" . $value['descripcion'] . "</p>
-    					<a href='#' class='btn btn-primary'>".$value["precio"]." €</a>
-  					</div>
-				</div>";
-				$cont1++;
-			}
-		}
-		echo"<h2>CIENCIA FICCION</h2>";
-		foreach ($biblioteca as $key => $value) {
-
-			if (strcmp($value["categoria"], "scifi") === 0 && $cont2<4) {
-				echo "<div class='card' style='width: 18rem; margin-left:40px;'>
-  					<img src='" . $value["foto"] . "' class='card-img-top' alt='...'>
-  					<div class='card-body'>
-   						<h5 class='card-title'>" . $value["titulo"] . "</h5>
-    					<p class='card-text'>" . $value['descripcion'] . "</p>
-    					<a href='#' class='btn btn-primary'>".$value["precio"]." €</a>
-  					</div>
-				</div>";
-				$cont2++;
-			}
-		}
-		echo"<h2>NOVELA</h2>";
-		foreach ($biblioteca as $key => $value) {
-
-			if (strcmp($value["categoria"], "novela") === 0 && $cont3<4) {
-				echo "<div class='card' style='width: 18rem; margin-left:40px;'>
-  					<img src='" . $value["foto"] . "' class='card-img-top' alt='...'>
-  					<div class='card-body'>
-   						<h5 class='card-title'>" . $value["titulo"] . "</h5>
-    					<p class='card-text'>" . $value['descripcion'] . "</p>
-    					<a href='#' class='btn btn-primary'>".$value["precio"]." €</a>
-  					</div>
-				</div>";
-				$cont3++;
-			}
-		}
-		
+		echo "<h1>LIBRERIA JAROSO 2022</h1>";
+		echo "<h2>TERROR</h2>";
+		pintarLibros($biblioteca, "terror");
+		echo "<h2>NOVELA</h2>";
+		pintarLibros($biblioteca, "novela");
+		echo "<h2>HISTORIA</h2>";
+		pintarLibros($biblioteca, "historia");
+		echo "<h2>CIENCIA FICCION</h2>";
+		pintarLibros($biblioteca, "scifi");
 		echo "</div>";
 		echo "</div>";
 		?>
--- tema2/practica2/eje8.php
+++ tema2/practica2/eje8.php
@@ -2,12 +2,69 @@
 include_once($_SERVER["DOCUMENT_ROOT"] . "/cabecera.php");
 ?>
 
-<div class="col-md-10 themed-grid-col">
-    <div class="p-3 bg-white">
+<div class="col-md-10 themed-grid-col" >
+    <div class="p-1"  style="background:url(./img8/bg.jpg);">
 
         <?php
+        $lolPersonajes = array(
+            array("id" => 1, "titulo" => "LA VENGANZA ARDIENTE", "nombre" => "BRAND", "descripcion" => "Brand, antiguo miembro de la tribu Kegan Rodhe del helado Freljord, es una lección sobre la tentación de un poder mayor. En busca de una de las legendarias Runas Geogénicas, Kegan traicionó a sus compañeros y se quedó con la runa. El hombre desapareció... ", "foto" => "./img8/brand.jpg", "imagenes" => array("habq" => "./img8/brand/BrandQ.png", "habw" => "./img8/brand/BrandW.png", "habe" => "./img8/brand/BrandE.png", "habr" => "./img8/brand/BrandR.png"), "bg" => "./img8/bg.jpg", "video"=>"https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0063/ability_0063_R1.webm"),
+            array("id" => 1, "titulo" => "EL CORAZÓN DE FRELJORD", "nombre" => "BRAUM", "descripcion" => "Bendecido con bíceps enormes y un corazón aún más grande, Braum es un héroe muy apreciado en Freljord. Todas las tabernas al norte del Fuerte Helado brindan por su fuerza legendaria. Se dice que taló un bosque de robles en una sola noche y convirtió una...", "foto" => "./img8/braum.jpg", "imagenes" => array("habq" => "./img8/braum/BraumQ.png", "habw" => "./img8/braum/BraumW.png", "habe" => "./img8/braum/BraumE.png", "habr" => "./img8/braum/BraumR.png"), "bg" => "./img8/bg.jpg", "video"=>"https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0201/ability_0201_R1.webm"),
+            array("id" => 1, "titulo" => "EL DESDÉN DE LA LUNA", "nombre" => "DIANA", "descripcion" => "Portadora de una espada en forma de media luna, Diana es una guerrera de los Lunari, una fe rechazada en casi todas las tierras a los pies del Monte Targon. Ataviada con una armadura reluciente del color de la nieve en una noche de invierno, es la... ", "foto" => "./img8/diana.jpg", "imagenes" => array("habq" => "./img8/diana/DianaQ.png", "habw" => "./img8/diana/DianaW.png", "habe" => "./img8/diana/DianaE.png", "habr" => "./img8/diana/DianaR.png"), "bg" => "./img8/bg.jpg", "video"=>"https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0131/ability_0131_R1.webm"),
+            array("id" => 1, "titulo" => "EL CHICO QUE QUEBRÓ EL TIEMPO", "nombre" => "EKKO", "descripcion" => "Ekko, un prodigio surgido de las implacables calles de Zaun, manipula el tiempo para sacar ventaja de todas las situaciones. Con una máquina de su invención llamada Dispositivo Z, explora las distintas posibilidades de la realidad hasta alcanzar el...", "foto" => "./img8/ekko.jpg", "imagenes" => array("habq" => "./img8/ekko/EkkoQ.png", "habw" => "./img8/ekko/EkkoW.png", "habe" => "./img8/ekko/EkkoE.png", "habr" => "./img8/ekko/EkkoR.png"), "bg" => "./img8/bg.jpg", "video"=>"https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0245/ability_0245_R1.webm"),
+            array("id" => 1, "titulo" => "EL GAMBERRO DE LAS MAREAS", "nombre" => "FIZZ", "descripcion" => "Fizz es un yordle anfibio que habita entre los arrecifes de alrededor de Aguas Estancadas. Suele recuperar y devolver los diezmos arrojados al mar por capitanes supersticiosos, pero incluso los marineros más agudos saben que no hay que plantarle cara...", "foto" => "./img8/fizz.jpg", "imagenes" => array("habq" => "./img8/fizz/FizzQ.png", "habw" => "./img8/fizz/FizzW.png", "habe" => "./img8/fizz/FizzE.png", "habr" => "./img8/fizz/FizzR.png"), "bg" => "./img8/bg.jpg", "video"=>"https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0105/ability_0105_R1.webm"),
+            array("id" => 1, "titulo" => "EL PEQUEÑO MAESTRO DEL MAL", "nombre" => "VEIGAR", "descripcion" => "Entusiasta maestro de la magia negra, Veigar ha hecho suyos poderes a los que pocos mortales se atreven a acercarse. Como espíritu de Ciudad de Bandle, buscó durante mucho tiempo deshacerse de las limitaciones de la magia yordle, así que su atención se...", "foto" => "./img8/veigar.jpg", "imagenes" => array("habq" => "./img8/veigar/VeigarQ.png", "habw" => "./img8/veigar/VeigarW.png", "habe" => "./img8/veigar/VeigarE.png", "habr" => "./img8/veigar/VeigarR.png"), "bg" => "./img8/bg.jpg", "video"=>"https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0045/ability_0045_R1.webm"),
+            array("id" => 1, "titulo" => "EL OJO DEL VACÍO", "nombre" => "VEL'KOZ", "descripcion" => "No es seguro que Vel'Koz sea el primer ente del Vacío que ha aparecido en Runaterra, pero no ha habido ningún otro con su nivel de crueldad y conciencia calculadora. Mientras que los de su especie devoran y profanan todo cuanto encuentran, él busca el...", "foto" => "./img8/velkoz.jpg", "imagenes" => array("habq" => "./img8/velkoz/VelkozQ.png", "habw" => "./img8/velkoz/VelkozW.png", "habe" => "./img8/velkoz/VelkozE.png", "habr" => "./img8/velkoz/VelkozR.png"), "bg" => "./img8/bg.jpg", "video"=>"https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0161/ability_0161_R1.webm")
+
+        );
+
+
+        echo "<div class='container'>";
+        echo "<div class='row g-5'>";
+        foreach ($lolPersonajes as $value) {
+            echo "<div class='card mb-3' style='width: 90%; height:625px; background:url(" . $value["bg"] . ");'>
+            <div class='row g-2'>
+              <div class='col-md-4'>
+                <img  src='" . $value["foto"] . "' style='width: 100%; height:75%; margin-top:70px' alt='Responsive image'>
+              </div>
+              <div class='col-md-8'>
+                <div class='card-body'>
+                  <h3  class='text-warning'>" . $value["titulo"] . "</h3>
+                  <h1 class='text-warning'>" . $value["nombre"] . "</h1>
+                  <p class='text-light'>" . $value["descripcion"] . "</p>
+                  
+                    <div class='container text-center'>
+                        <video autoplay loop preload muted height='300' style='text-align:center';>
+                        <source src='".$value["video"]."' type='video/webm' />
+                        </video>
+                    </div>";
+                    echo "<table class='table  black-500' style='border:none;'>";
+                    echo "<tr>";
+                    foreach($value['imagenes'] as $img) {
+                        echo "<td>";
+                        echo "<img  src='".$img."' alt=''>";
+                        echo "</td>";
+                    }
+                    echo "</tr>";
+                    echo "</table>";
+               echo" </div>
+              </div>
+            </div>
+          </div>";
+        }
+        echo "</div>";
+        echo "</div>";
+
+
+
+
+
+
+
+
+
 
         ?>
+
 
     </div>
 </div>
