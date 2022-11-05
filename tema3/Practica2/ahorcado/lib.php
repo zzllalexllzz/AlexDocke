fb256c5878da2ee63f927a841450e7439fb517d8
unknown <alexander.br159@gmail.com>
2022-11-05 17:26:13 +0100

    Tema3 ahorcado terminado5

--- tema3/Practica2/ahorcado/lib.php
+++ tema3/Practica2/ahorcado/lib.php
@@ -4,7 +4,7 @@ function palabraRand() {
      //contiene todas la palabras que usaremos en el array
     $diccionario=["ABEJA", "ABEJORRO", "AGUILA", "ALMEJA", "ANACONDA", "ARAÑA", "ASNO", "ATUN", "AVESTRUZ", "AVISPA", "BALLENA", "BESUGO", "BUFALO", "BUHO", "BUITRE", "BURRO", "CABALLO", "CABRA", "CAIMAN", "CAMALEON", "CAMELLO", "CANARIO", "CANGREJO", "CARACOL", "CEBRA", "CERDO", "CIERVO", "CIGALA", "COBRA", "CONEJO", "COTORRA", "COYOTE", "DELFIN", "ELEFANTE", "FOCA", "GACELA", "GALLO", "GACELA", "GARZA", "GATO", "GAVILAN", "GAVIOTA", "GORILA", "GORRION", "GRILLO", "GUEPARDO", "GUSANO", "HALCON", "HAMSTER", "HIENA", "HORMIGA", "IGUANA", "JABALI", "JAGUAR", "JINETA", "JIRAFA", "KOALA", "LAGARTO", "LANGOSTA", "LECHUZA", "LEON", "LEOPARDO", "LEMUR", "LIBELULA", "LIEBRE", "LINCE", "LLAMA", "LOBO", "LOMBRIZ", "LORO", "MARIPOSA", "MARMOTA", "MARSOPA", "MEJILLON", "MONO", "MOSCA", "MULA", "NUTRIA", "ORCA", "OSO", "OSTRA", "OVEJA", "PALOMA", "PANTERA", "PATO", "PAVO", "PERDIZ", "PERRO", "PINGUINO", "PITON", "PULGA", "PULPO", "PUMA", "RANA", "RATON", "SALMON", "SAPO", "TIBURON", "TIGRE", "TOPO", "TORO", "TORTUGA", "TRUCHA", "TUCAN", "VACA", "VIBORA", "ZORRO"];
     $palabra=$diccionario[rand(0,6)];
-    $palabra=strtoupper($palabra);
+    $palabra=$palabra;
     return str_split($palabra);
 }
 //metodo que pasa la palabra y lo pasa en guones(-) el total de deltras de la palabra
