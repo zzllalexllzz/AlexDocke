06aaec94dfbf8eb8e4deaf41bd5db10b221a41ad
zzllalexllzz <alexander.br159@gmail.com>
2022-12-07 16:50:54 +0100

    prepa

--- tema6/Practica1/cabecera.php
+++ tema6/Practica1/cabecera.php
@@ -130,7 +130,7 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             
-                            <img class='img-profile rounded-circle' src='img/undraw_profile.svg'>  
+                            <img class='img-profile rounded-circle' src='style/img/undraw_profile.svg'>  
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
